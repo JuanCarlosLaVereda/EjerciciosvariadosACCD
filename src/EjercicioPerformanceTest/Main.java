7cb5ba629d6ca2e5e3a51eca1bb035a8c0ead009
jsantamariap <juasanper2@alu.edu.gva.es>
2025-11-10 21:19:54 +0100

    Metodo 1 terminado

---
+++ src/EjercicioPerformanceTest/Main.java
@@ -0,0 +1,7 @@
+package EjercicioPerformanceTest;
+
+public class Main {
+    static void main() {
+
+    }
+}
---
+++ src/EjercicioPerformanceTest/TestRendimientoPool.java
@@ -0,0 +1,90 @@
+package EjercicioPerformanceTest;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.sql.*;
+
+public class TestRendimientoPool {
+
+    private static final String DB_URL =  "jdbc:mysql://localhost:3306/";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "1234";
+    private static final int NUM_PETICIONES = 5000;
+    private static final String CONSULTA = "SELECT 1";
+
+
+    static void main() {
+        //Metodo 1
+        testConDriverManager();
+        System.out.println("Metodo 2");
+        long tiempo = testConDriverManager();
+        System.out.println("Tiempo de ejecucion: " + tiempo + "ms");
+
+
+
+/*        //Metodo 2
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(DB_URL);
+        config.setUsername(DB_USER);
+        config.setPassword(DB_PASSWORD);
+        config.setMaximumPoolSize(20);
+        config.setMinimumIdle(5);
+        config.setConnectionTimeout(30000); //30 segundos
+
+
+        try (HikariDataSource ds = new HikariDataSource(config);){
+            System.out.println("Metodo 2");
+            tiempo = testConDataSource(ds);
+            System.out.println("Tiempo de ejecucion: " + tiempo + "ms");
+        }*/
+
+    }
+
+    public static long testConDriverManager(){
+        long tiempo_inicio = System.nanoTime();
+        for(int i = 0; i < NUM_PETICIONES; i++){
+            try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            PreparedStatement ps =  con.prepareStatement(CONSULTA);
+            ResultSet rs = ps.executeQuery();){
+
+                if(rs.next()){
+                    System.out.println(rs.getString(1));
+                }
+
+
+            } catch (SQLException e) {
+                System.err.println("Error al conectar con la base de datos");
+                e.printStackTrace();
+            }
+        }
+        long tiempo_final = System.nanoTime();
+        return ((tiempo_final - tiempo_inicio) / 1_000_000);
+
+    }
+    public static long testConDataSource(HikariDataSource ds) {
+        System.out.println("Pool creado");
+        long tiempo_inicio = System.nanoTime();
+        for (int i = 1; i <= NUM_PETICIONES; i++) {
+            try (Connection connection = ds.getConnection();){
+
+                try (PreparedStatement ps =  connection.prepareStatement(CONSULTA);
+                     ResultSet rs = ps.executeQuery()) {
+
+                    if (rs.next()) {
+                        System.out.println(rs.getString(1));
+                    }
+                }
+
+
+            } catch (SQLException e) {
+                System.err.println("Error al conectar con la base de datos");
+                e.printStackTrace();
+            }
+        }
+        long tiempo_final = System.nanoTime();
+        return ((tiempo_final - tiempo_inicio) / 1_000_000);
+    }
+}
